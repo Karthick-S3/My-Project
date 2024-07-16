b2f2855921b839251d733e8faab470e4e904537f
Swetha Senthilkumar <146800600+SWETHA-S2224@users.noreply.github.com>
2024-07-16 17:43:29 +0530

    Add files via upload

---
+++ src/app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
